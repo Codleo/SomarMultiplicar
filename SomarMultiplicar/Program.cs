a9d2a487a11ffc8a28b07e5cb63aafee3764117a
Leonardo dos Santos <leo.santos2001@hotmail.com>
2024-06-20 15:28:11 -0300

    Adicionar arquivos de projeto.

---
+++ SomarMultiplicar/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int numero1 = 10;
+        int numero2 = 5;
+
+        var resultado = SomarEMultiplicar(numero1, numero2);
+
+        Console.WriteLine($"Soma: {resultado.Item1}");
+        Console.WriteLine($"Multiplicação: {resultado.Item2}");
+    }
+
+
+    public static (int, int) SomarEMultiplicar(int a, int b)
+    {
+        int soma = a + b;
+        int multiplicacao = a * b;
+
+        return (soma, multiplicacao);
+    }
+}
